e4fb7f954d2b22bcc5081be4c36a2703db1184c0
James Couball <jcouball@yahoo.com>
2023-01-28 09:58:07 -0800

    Add hello world feature (#8)

--- spec/create_github_release_test_spec.rb
+++ spec/create_github_release_test_spec.rb
@@ -4,4 +4,10 @@ RSpec.describe CreateGithubReleaseTest do
   it "has a version number" do
     expect(CreateGithubReleaseTest::VERSION).not_to be nil
   end
+
+  describe '.hello' do
+    it 'should output "Hello, World!"' do
+      expect { CreateGithubReleaseTest.hello }.to output("Hello, World!\n").to_stdout
+    end
+  end
 end
