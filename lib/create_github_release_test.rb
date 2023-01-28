884196c653f76d80f02c1c1be64c4b98c49b1c50
James Couball <jcouball@yahoo.com>
2023-01-28 09:57:21 -0800

    Add hello world feature

--- lib/create_github_release_test.rb
+++ lib/create_github_release_test.rb
@@ -4,5 +4,8 @@ require_relative "create_github_release_test/version"
 
 module CreateGithubReleaseTest
   class Error < StandardError; end
-  # Your code goes here...
+
+  def self.hello
+    puts "Hello, World!"
+  end
 end
